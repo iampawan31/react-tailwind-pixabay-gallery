0fdf4258aae5ea82f04ed6abae80b2824f1ebade
Pawan Kumar <surpawan@gmail.com>
2022-03-04 21:48:41 +0530

    Added changes

---
+++ src/Maintenance.js
@@ -0,0 +1,9 @@
+const Maintenance = () => {
+  return (
+    <div>
+      <h1>Maintance Mode. Please come back later</h1>
+    </div>
+  );
+};
+
+export default Maintenance;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './assets/main.css';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './assets/main.css'
+import App from './App'
+import Maintenance from './Maintenance'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {process.env.APP_ENV === 'maintenance' ? <Maintenance /> : <App />}
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
